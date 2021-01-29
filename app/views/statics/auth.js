cf985ae35ef05878d84b738d5b0855dd7bb354c9
sillybread <quoctoan.c2b@gmail.com>
2021-01-29 21:40:38 +0700

    validation

--- app/views/statics/auth.js
+++ app/views/statics/auth.js
@@ -29,12 +29,16 @@ function signupHandle() {
     _idToElement("usr").focus();
     _idToElement("signupForm").onsubmit = (e) => {
         e.preventDefault();
-        console.log(e);
-        let sData = `username=${_idToElement("usr").value}&` +
-            `password=${_idToElement("pwd").value}&` +
-            `email=${_idToElement("eml").value}`;
-        //let eMessage = _idToElement("message")
-        //eMessage.innerText = "Đang đăng nhập...";
+        let usr = _idToElement("usr").value;
+        let pwd = _idToElement("pwd").value;
+        let eml = _idToElement("eml").value;
+        let eMessage = _idToElement("message")
+        if (!(usr && pwd && eml)) {
+            eMessage.innerText = "Hãy điền vào tất cả các trường!";
+            return;
+        }
+        let sData = `username=${usr}&password=${pwd}&email=${eml}`;
+        eMessage.innerText = "Đang đăng ký...";
         fetch(e.target.action, {
             method: "POST",
             body: sData,
@@ -43,10 +47,10 @@ function signupHandle() {
             }
         }).then(res => {
             if (res.status === 200) {
-                //eMessage.innerText = "Đăng nhập thành công"
+                eMessage.innerText = "Đăng ký thành công"
                 window.location.href = "/signin.html"
             } else {
-                //eMessage.innerText = "Tài khoản hoặc mật khẩu không chính xác"
+                eMessage.innerText = "Tài khoản đã tồn tại trong hệ thống"
             }
         })
     }
