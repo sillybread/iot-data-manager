19f798e878f229a4f20d38b448bcd4dfadeeeb87
sillybread <quoctoan.c2b@gmail.com>
2021-01-29 21:31:26 +0700

    wrote sign up page

--- app/views/statics/auth.js
+++ app/views/statics/auth.js
@@ -1,22 +1,53 @@
-document.querySelector("[name=username]").focus();
-document.getElementById("loginForm").onsubmit = (e) => {
-    e.preventDefault();
-    let sData = `username=${document.getElementById('usr').value}&` +
-        `password=${document.getElementById('pwd').value}`;
-    let eMessage = document.getElementById("message")
-    eMessage.innerText = "Đang đăng nhập...";
-    fetch(e.target.action, {
-        method: 'POST',
-        body: sData,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    }).then(res => {
-        if (res.status === 200) {
-            eMessage.innerText = "Đăng nhập thành công"
-            window.location.href = "/"
-        } else {
-            eMessage.innerText = "Tài khoản hoặc mật khẩu không chính xác"
-        }
-    })
+const _idToElement = (id) => document.getElementById(id);
+
+function signinHandle() {
+    _idToElement("usr").focus();
+    _idToElement("signinForm").onsubmit = (e) => {
+        e.preventDefault();
+        let sData = `username=${_idToElement("usr").value}&` +
+            `password=${_idToElement("pwd").value}`;
+        let eMessage = _idToElement("message")
+        eMessage.innerText = "Đang đăng nhập...";
+        fetch(e.target.action, {
+            method: "POST",
+            body: sData,
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        }).then(res => {
+            if (res.status === 200) {
+                eMessage.innerText = "Đăng nhập thành công"
+                window.location.href = "/"
+            } else {
+                eMessage.innerText = "Tài khoản hoặc mật khẩu không chính xác"
+            }
+        })
+    }
+}
+
+function signupHandle() {
+    _idToElement("usr").focus();
+    _idToElement("signupForm").onsubmit = (e) => {
+        e.preventDefault();
+        console.log(e);
+        let sData = `username=${_idToElement("usr").value}&` +
+            `password=${_idToElement("pwd").value}&` +
+            `email=${_idToElement("eml").value}`;
+        //let eMessage = _idToElement("message")
+        //eMessage.innerText = "Đang đăng nhập...";
+        fetch(e.target.action, {
+            method: "POST",
+            body: sData,
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        }).then(res => {
+            if (res.status === 200) {
+                //eMessage.innerText = "Đăng nhập thành công"
+                window.location.href = "/signin.html"
+            } else {
+                //eMessage.innerText = "Tài khoản hoặc mật khẩu không chính xác"
+            }
+        })
+    }
 }
