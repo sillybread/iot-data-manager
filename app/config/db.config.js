15ebacbe49432d01bd3682c378e200a295916e5b
sillybread <quoctoan.c2b@gmail.com>
2021-01-29 08:54:10 +0700

    u

---
+++ app/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    URI: "mongodb://nqtoan:231199@sensorlog-shard-00-00.smke7.mongodb.net:27017,sensorlog-shard-00-01.smke7.mongodb.net:27017,sensorlog-shard-00-02.smke7.mongodb.net:27017/iotctu_db?ssl=true&replicaSet=atlas-39t7xo-shard-0&authSource=admin&retryWrites=true&w=majority"
+};
